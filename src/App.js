bd2d394091f471568ff79989161498ad35d56bb8
Odin <asal.karimi.b@gmail.com>
2022-06-12 22:40:10 +0430

    Add h1 and Update Footer Links

--- src/App.js
+++ src/App.js
@@ -5,14 +5,22 @@ import Dictionary from "./Dictionary";
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
       <div className="container">
-        <Dictionary />
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+        <div className=" dictionary-wrap">
+          <h1 className="text-center mt-4 mb-4">
+            What word are you looking for?
+          </h1>
+          <Dictionary />
+        </div>
       </div>
       <footer className="text-center text-muted mt-2 mb-2">
-        Coded by Asal Karimi
+        This project was coded by Asal Karimi and is open-sourced on{" "}
+        <a href="https://github.com/Odinkrm/dictionary-react-app">GitHub</a> and
+        hosted on{" "}
+        <a href="https://this-react-dictionary-app.netlify.app/">Netlify</a>
       </footer>
     </div>
   );
